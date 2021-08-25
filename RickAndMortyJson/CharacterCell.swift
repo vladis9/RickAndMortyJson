0d96b34069a61fe46714cf049bbd1e5abcc4e22c
Vlad Zinevich <vladisznv@gmail.com>
2021-08-25 15:37:44 +0300

    Initial Commit

---
+++ RickAndMortyJson/CharacterCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCell.swift
+//  RickAndMortyJson
+//
+//  Created by Vlad Zinevich on 25.08.2021.
+//
+
+import UIKit
+
+class CharacterCell: UITableViewCell {
+
+    @IBOutlet var characterUmage: UIImageView!
+    @IBOutlet var characterName: UILabel!
+    @IBOutlet var characterStatus: UILabel!
+    @IBOutlet var characterGender: UILabel!
+    
+}
---
+++ RickAndMortyJson/UserActionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionViewCell.swift
+//  RickAndMortyJson
+//
+//  Created by Vlad Zinevich on 25.08.2021.
+//
+
+import UIKit
+
+class UserActionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+}
